46d52c321d020ac189608cf2d96982edf3e6015b
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-24 14:00:26 +0530

    methods added

--- src/main/java/com/stackroute/pe5/CountString.java
+++ src/main/java/com/stackroute/pe5/CountString.java
@@ -1,12 +1,39 @@
 package com.stackroute.pe5;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountString {
+    Map<String, Integer> data = new HashMap<>();
+
+    @Override
+    public String toString() {
+        return "CountString{" +
+                "data=" + data +
+                '}';
+    }
 
     public String checkingFrequency(String input){
 
 
 
-        return null;
+        List<String> arrayofStrings = Arrays.asList(input.split("[\\W_]+"));
+
+        for (String fields : arrayofStrings) {
+
+            if (data.containsKey(fields)) {
+
+                data.replace(fields, 1 + data.get(fields));
+            }
+            else {
+
+                data.put(fields, 1);
+            }
+        }
+        return toString();
     }
+
 }
