5172516615ddc7e894b5550aeceda8febc93e0dd
adithyaGowda <adithyagowda5696@gmail.com>
2019-07-24 14:08:38 +0530

    methods added

---
+++ src/test/java/com/stackroute/pe5/CountStringTest.java
@@ -0,0 +1,27 @@
+package com.stackroute.pe5;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CountStringTest {
+    CountString countString;
+
+    @Before
+    public void setUp() throws Exception {
+        countString = new CountString();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        countString = null;
+    }
+
+    @Test
+    public void givenInputStringShouldReturnFrequency(){
+
+        assertEquals("CountString{data={one=5, two=2, three=2}}", countString.checkingFrequency("one one -one___two,,three,one @three*one?two"));
+    }
+}
